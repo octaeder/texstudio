2081799abb752d1f960e3668016dd83c8aaae7c5
octaeder <102688820+octaeder@users.noreply.github.com>
2023-07-02 15:24:24 +0200

    show process Time

--- src/texstudio.cpp
+++ src/texstudio.cpp
@@ -69,6 +69,7 @@
 #include "symbollistmodel.h"
 #include "symbolwidget.h"
 #include "execprogram.h"
+#include <QTime>
 
 #include <QScreen>
 
@@ -6595,7 +6596,8 @@ void Texstudio::processNotification(const QString &message)
 {
 	if (message.startsWith(tr("Error:")))
 		outputView->showPage(outputView->MESSAGES_PAGE);
-	outputView->insertMessageLine(message + "\n");
+	QString timeInfo = QTime::currentTime().toString("hh:mm:ss");
+	outputView->insertMessageLine(timeInfo + " " + message + "\n");
 }
 /*!
  * \brief clear log view in panel
